9b7979f700139874fe05ec887cd4791f6163ad6b
ViktoriiaTrostohon <84284942+ViktoriiaTrostohon@users.noreply.github.com>
2021-08-14 01:57:17 +0300

    js kill

--- js/video.js
+++ js/video.js
@@ -1,11 +1,11 @@
 const videoPlayer = document.querySelector('.video-player')
 const video = videoPlayer.querySelector('.video')
 const playButton = videoPlayer.querySelector('.play-button')
-const volume = videoPlayer.querySelector('.volume')
-const currentTimeElement = videoPlayer.querySelector('.current')
-const durationTimeElement = videoPlayer.querySelector('.duration')
-const progress = videoPlayer.querySelector('.video-progress')
-const progressBar = videoPlayer.querySelector('.video-progress-filled')
+// const volume = videoPlayer.querySelector('.volume')
+// const currentTimeElement = videoPlayer.querySelector('.current')
+// const durationTimeElement = videoPlayer.querySelector('.duration')
+// const progress = videoPlayer.querySelector('.video-progress')
+// const progressBar = videoPlayer.querySelector('.video-progress-filled')
 
 
 //Play and Pause button
@@ -20,32 +20,32 @@ playButton.addEventListener('click', (e) => {
 })
 
 //volume
-volume.addEventListener('mousemove', (e)=> {
-  video.volume = e.target.value
-})
+// volume.addEventListener('mousemove', (e)=> {
+//   video.volume = e.target.value
+// })
 
 //current time and duration
-const currentTime = () => {
-  let currentMinutes = Math.floor(video.currentTime / 60)
-  let currentSeconds = Math.floor(video.currentTime - currentMinutes * 60)
-  let durationMinutes = Math.floor(video.duration / 60)
-  let durationSeconds = Math.floor(video.duration - durationMinutes * 60)
+// const currentTime = () => {
+//   let currentMinutes = Math.floor(video.currentTime / 60)
+//   let currentSeconds = Math.floor(video.currentTime - currentMinutes * 60)
+//   let durationMinutes = Math.floor(video.duration / 60)
+//   let durationSeconds = Math.floor(video.duration - durationMinutes * 60)
 
-  currentTimeElement.innerHTML = `${currentMinutes}:${currentSeconds < 10 ? '0'+currentSeconds : currentSeconds}`
-  durationTimeElement.innerHTML = `${durationMinutes}:${durationSeconds}`
-}
+//   currentTimeElement.innerHTML = `${currentMinutes}:${currentSeconds < 10 ? '0'+currentSeconds : currentSeconds}`
+//   durationTimeElement.innerHTML = `${durationMinutes}:${durationSeconds}`
+// }
 
-video.addEventListener('timeupdate', currentTime)
+// video.addEventListener('timeupdate', currentTime)
 
 
 //Progress bar
-video.addEventListener('timeupdate', () =>{
-  const percentage = (video.currentTime / video.duration) * 100
-  progressBar.style.width = `${percentage}%`
-})
+// video.addEventListener('timeupdate', () =>{
+//   const percentage = (video.currentTime / video.duration) * 100
+//   progressBar.style.width = `${percentage}%`
+// })
 
 //change progress bar on click
-progress.addEventListener('click', (e) =>{
-  const progressTime = (e.offsetX / progress.offsetWidth) * video.duration
-  video.currentTime = progressTime
-})
+// progress.addEventListener('click', (e) =>{
+//   const progressTime = (e.offsetX / progress.offsetWidth) * video.duration
+//   video.currentTime = progressTime
+// })
