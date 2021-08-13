6a8280789987e76109610a6433154a5278303758
ViktoriiaTrostohon <84284942+ViktoriiaTrostohon@users.noreply.github.com>
2021-08-13 19:49:16 +0300

    custom button

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 <p>
               Приємного перегляду!<span class="winking-face">&#128521;</span>
+
+<svg class="video-button" width="67" height="67">
+                    <use href="./images/sprite.svg#icon-play-button"></use>
+                  </svg>
---
+++ js/video.js
@@ -0,0 +1,16 @@
+const videoPlayer = document.querySelector('.video-player')
+const video = videoPlayer.querySelector('.video')
+const playButton = videoPlayer.querySelector('.play-button')
+
+
+
+//Play and Pause button
+playButton.addEventListener('click', (e) => {
+  if(video.paused){
+    video.play()
+    e.target.textContent = '||'
+  } else {
+    video.pause()
+    e.target.textContent = '▶'
+  }
+})
